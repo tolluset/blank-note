e2c41d9089f794944c259deb9763a0df7ea58670
tolluset <dlqud19@gmail.com>
2025-08-11 04:19:55 +0900

    fix: cors

--- packages/server/src/index.ts
+++ packages/server/src/index.ts
@@ -1,8 +1,15 @@
 import { Hono } from 'hono'
+import { cors } from 'hono/cors'
 import { serve } from '@hono/node-server'
 import notes from './notes'
 
 const app = new Hono()
+
+app.use('*', cors({
+  origin: '*',
+  allowHeaders: ['Content-Type', 'Authorization'],
+  allowMethods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'],
+}))
 
 app.get('/', (c) => {
   return c.text('Hello Hono!')
