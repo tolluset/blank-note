cbff941e6255db606d19893b26e34ba8ef7cb415
tolluset <dlqud19@gmail.com>
2025-08-11 18:58:34 +0900

    fix: server start

--- packages/server/src/db/index.ts
+++ packages/server/src/db/index.ts
@@ -1,8 +1,5 @@
 import { drizzle } from 'drizzle-orm/node-postgres';
-import dotenv from 'dotenv';
 
-dotenv.config({ path: '.env' });
-
-const DATABASE_URL = `postgres://${process.env.POSTGRES_USER}:${process.env.POSTGRES_PASSWORD}@localhost:${process.env.POSTGRES_PORT}/${process.env.POSTGRES_DB}`;
+const DATABASE_URL = process.env.DATABASE_URL || `postgres://${process.env.POSTGRES_USER}:${process.env.POSTGRES_PASSWORD}@postgres:5432/${process.env.POSTGRES_DB}`;
 
 export const db = drizzle(DATABASE_URL);
